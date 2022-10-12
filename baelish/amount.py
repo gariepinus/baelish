81bffe43d4ba63f700b7c686420f36a87e4f6648
gariepinus <mail@gariepinus.de>
2022-10-12 20:16:59 +0200

    Attribute name refactoring
    gold_dragons => _goldDragons
    silver_stags => _silverStags
    copper_pennies => _copperPennies

--- baelish/amount.py
+++ baelish/amount.py
@@ -2,24 +2,24 @@ class Amount:
     """An amount of SIFRP currency represented in Gold Dragons (gd), Silver Stags (ss) and Copper Pennies (cp)."""
 
     def __init__(self, gd=0, ss=0, cp=0):
-        self.gold_dragons = Coin("Gold Dragons", "GD", gd)
-        self.silver_stags = Coin("Silver Stags", "SS", ss)
-        self.copper_pennies = Coin("Copper Pennies", "CP", cp)
+        self._goldDragons = Coin("Gold Dragons", "GD", gd)
+        self._silverStags = Coin("Silver Stags", "SS", ss)
+        self._copperPennies = Coin("Copper Pennies", "CP", cp)
 
     def __repr__(self):
-        return "<Amount: {}, {}, {}>".format(self.gold_dragons, self.silver_stags, self.copper_pennies)
+        return "<Amount: {}, {}, {}>".format(self._goldDragons, self._silverStags, self._copperPennies)
 
     def __str__(self, useLongUnits=False, format_specifier=".2g"):
         output=[]
 
-        if self.gold_dragons != 0:
-            output.append(self.gold_dragons.__str__(useLongUnits, format_specifier))
+        if self._goldDragons != 0:
+            output.append(self._goldDragons.__str__(useLongUnits, format_specifier))
 
-        if self.silver_stags != 0:
-            output.append(self.silver_stags.__str__(useLongUnits, format_specifier))
+        if self._silverStags != 0:
+            output.append(self._silverStags.__str__(useLongUnits, format_specifier))
 
-        if self.copper_pennies != 0 or (self.gold_dragons == 0 and self.silver_stags == 0):
-            output.append(self.copper_pennies.__str__(useLongUnits, format_specifier))
+        if self._copperPennies != 0 or (self._goldDragons == 0 and self._silverStags == 0):
+            output.append(self._copperPennies.__str__(useLongUnits, format_specifier))
 
         return ", ".join(output)
 

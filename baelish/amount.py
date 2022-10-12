c4c9207aa0ee9c20bf67137b331c56cdca52a0e7
gariepinus <mail@gariepinus.de>
2022-10-12 20:13:03 +0200

    Coin class

--- baelish/amount.py
+++ baelish/amount.py
@@ -2,23 +2,35 @@ class Amount:
     """An amount of SIFRP currency represented in Gold Dragons (gd), Silver Stags (ss) and Copper Pennies (cp)."""
 
     def __init__(self, gd=0, ss=0, cp=0):
-        self.gold_dragons = gd
-        self.silver_stags = ss
-        self.copper_pennies = cp
+        self.gold_dragons = Coin("Gold Dragons", "GD", gd)
+        self.silver_stags = Coin("Silver Stags", "SS", ss)
+        self.copper_pennies = Coin("Copper Pennies", "CP", cp)
 
     def __repr__(self):
-        return "<Amount: {} GD, {} SS, {} CP>".format(self.gold_dragons, self.silver_stags, self.copper_pennies)
+        return "<Amount: {}, {}, {}>".format(self.gold_dragons, self.silver_stags, self.copper_pennies)
 
-    def __str__(self, format_specifier=".2f"):
+    def __str__(self, useLongUnits=False, format_specifier=".2g"):
         output=[]
 
         if self.gold_dragons != 0:
-            output.append("{:{format_specifier}} GD".format(self.gold_dragons, format_specifier=format_specifier))
+            output.append(self.gold_dragons.__str__(useLongUnits, format_specifier))
 
         if self.silver_stags != 0:
-            output.append("{:{format_specifier}} SS".format(self.silver_stags, format_specifier=format_specifier))
+            output.append(self.silver_stags.__str__(useLongUnits, format_specifier))
 
         if self.copper_pennies != 0 or (self.gold_dragons == 0 and self.silver_stags == 0):
-            output.append("{:{format_specifier}} CP".format(self.copper_pennies, format_specifier=format_specifier))
+            output.append(self.copper_pennies.__str__(useLongUnits, format_specifier))
 
         return ", ".join(output)
+
+
+class Coin(float):
+    def __new__(self, longUnit, shortUnit, quantity=0):
+        return super().__new__(self, quantity)
+
+    def __init__(self, longUnit, shortUnit, quantity=0):
+        self._longUnit = longUnit
+        self._shortUnit = shortUnit
+
+    def __str__(self, useLongUnit=False, formatSpecifier=".2g"):
+        return "{:{formatSpecifier}} {}".format(self, self._longUnit if useLongUnit else self._shortUnit, formatSpecifier=formatSpecifier)

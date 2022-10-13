efd33d3d0f40dfd4869cabf1313e91b34924c966
gariepinus <mail@gariepinus.de>
2022-10-13 19:52:40 +0200

    Revert "Use round instead of format specifier"
    This reverts commit ee2ac67f6ae7fcee7d422e13d7e773ec6754785e.

--- baelish/amount.py
+++ baelish/amount.py
@@ -9,17 +9,17 @@ class Amount:
     def __repr__(self):
         return "<Amount: {}, {}, {}>".format(self._goldDragons, self._silverStags, self._copperPennies)
 
-    def __str__(self, useLongUnits=False, maxDecimals=2):
+    def __str__(self, useLongUnits=False, format_specifier=".2g"):
         output=[]
 
         if self._goldDragons != 0:
-            output.append(self._goldDragons.__str__(useLongUnits, maxDecimals))
+            output.append(self._goldDragons.__str__(useLongUnits, format_specifier))
 
         if self._silverStags != 0:
-            output.append(self._silverStags.__str__(useLongUnits, maxDecimals))
+            output.append(self._silverStags.__str__(useLongUnits, format_specifier))
 
         if self._copperPennies != 0 or (self._goldDragons == 0 and self._silverStags == 0):
-            output.append(self._copperPennies.__str__(useLongUnits, maxDecimals))
+            output.append(self._copperPennies.__str__(useLongUnits, format_specifier))
 
         return ", ".join(output)
 
@@ -69,5 +69,5 @@ class Coin(float):
         self._longUnit = longUnit
         self._shortUnit = shortUnit
 
-    def __str__(self, useLongUnit=False, maxDecimals=2):
-        return "{} {}".format(round(self, maxDecimals), self._longUnit if useLongUnit else self._shortUnit)
+    def __str__(self, useLongUnit=False, formatSpecifier=".2g"):
+        return "{:{formatSpecifier}} {}".format(self, self._longUnit if useLongUnit else self._shortUnit, formatSpecifier=formatSpecifier)

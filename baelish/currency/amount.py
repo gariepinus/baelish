f009db7a8304bfdf028ad4a882f3101fd4d03cf9
gariepinus <mail@gariepinus.de>
2022-10-17 19:31:18 +0200

    Use quantity in amount
    Update quantity for changes in commit 0022dfe97b872b56ebe4f98f116d34fb9f05bb40.

--- baelish/currency/amount.py
+++ baelish/currency/amount.py
@@ -1,26 +1,26 @@
+from .quantity import Quantity
+
 class Amount:
     """An amount of SIFRP currency represented in Gold Dragons (gd), Silver Stags (ss) and Copper Pennies (cp)."""
 
     def __init__(self, gd=0, ss=0, cp=0):
-        self._goldDragons = Coin("Gold Dragons", "GD", gd)
-        self._silverStags = Coin("Silver Stags", "SS", ss)
-        self._copperPennies = Coin("Copper Pennies", "CP", cp)
+        self._gold_dragons = Quantity(gd, "Gold Dragons", "GD")
+        self._silver_stags = Quantity(ss, "Silver Stags", "SS")
+        self._copper_pennies = Quantity(cp, "Copper Pennies", "CP")
 
     def __repr__(self):
-        return "<Amount: {}, {}, {}>".format(self._goldDragons, self._silverStags, self._copperPennies)
+        return "<Amount: {}, {}, {}>".format(self._gold_dragons, self._silver_stags, self._copper_pennies)
 
-    def __str__(self, useLongUnits=False, format_specifier=".2f"):
+    def __str__(self, use_abbrev=True, format_specifier=".0f"):
         output=[]
 
-        if self._goldDragons != 0:
-            output.append(self._goldDragons.__str__(useLongUnits, format_specifier))
-
-        if self._silverStags != 0:
-            output.append(self._silverStags.__str__(useLongUnits, format_specifier))
-
-        if self._copperPennies != 0 or (self._goldDragons == 0 and self._silverStags == 0):
-            output.append(self._copperPennies.__str__(useLongUnits, format_specifier))
-
+        if self._gold_dragons != 0:
+            output.append(self._gold_dragons.__str__(use_abbrev, format_specifier))
+        if self._silver_stags != 0:
+            output.append(self._silver_stags.__str__(use_abbrev, format_specifier))
+        if self._copper_pennies != 0 or (self._gold_dragons == 0 and self._silver_stags == 0):
+            output.append(self._copper_pennies.__str__(use_abbrev, format_specifier))
+        
         return ", ".join(output)
 
     def __int__(self):
@@ -49,40 +49,40 @@ class Amount:
 
     @property
     def gd(self):
-        return self._goldDragons
+        return self._gold_dragons
 
     @gd.setter
     def gd(self, value):
-        self._goldDragons = Coin("Gold Dragons", "GD", value)
+        self._gold_dragons = Quantity(value, "Gold Dragons", "GD")
 
     @property
     def in_gd(self):
-        return Coin("Gold Dragons", "GD", self.in_ss / 210)
+        return Quantity(self.in_ss / 210, "Gold Dragons", "GD")
 
     @property
     def ss(self):
-        return self._silverStags
+        return self._silver_stags
 
     @ss.setter
     def ss(self, value):
-        self._silverStags = Coin("Silver Stags", "SS", value)
+        self._silver_stags = Quantity(value, "Silver Stags", "SS")
 
     @property
     def in_ss(self):
-        return Coin("Silver Stags", "SS", self.in_cp / 56)
+        return Quantity(self.in_cp / 56, "Silver Stags", "SS")
 
     @property
     def cp(self):
-        return self._copperPennies
+        return self._copper_pennies
 
     @cp.setter
     def cp(self, value):
-        self._copperPennies = Coin("Copper Pennies", "CP", value)
+        self._copper_pennies = Quantity(value, "Copper Pennies", "CP")
 
     @property
     def in_cp(self):
-        value = (((self._goldDragons * 210) + self._silverStags) * 56) + self._copperPennies
-        return Coin("Copper Pennies", "CP", value)
+        value = (((self._gold_dragons * 210) + self._silver_stags) * 56) + self._copper_pennies
+        return Quantity(value, "Copper Pennies", "CP")
 
     @property
     def minimized(self):
@@ -92,17 +92,17 @@ class Amount:
             original = original * -1
         
         diff = original % (210 * 56)
-        goldDragons = (original - diff) / (210 * 56)
+        gold_dragons = (original - diff) / (210 * 56)
 
         original = diff
         diff = original % 56
         
-        silverStags = (original - diff) / 56
-        copperPennies = diff
+        silver_stags = (original - diff) / 56
+        copper_pennies = diff
 
         if self < 0:
-            goldDragons = goldDragons * -1
-            silverStags = silverStags * -1
-            copperPennies = copperPennies * -1
+            gold_dragons = gold_dragons * -1
+            silver_stags = silver_stags * -1
+            copper_pennies = copper_pennies * -1
 
-        return Amount(goldDragons, silverStags, copperPennies)
+        return Amount(gold_dragons, silver_stags, copper_pennies)

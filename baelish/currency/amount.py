570866f627c0c2e6ac6039750e367dd5caa3227b
gariepinus <mail@gariepinus.de>
2022-10-17 18:49:53 +0200

    Restructure
    Sub dir for currency related classes and own file for coin class.

--- baelish/currency/amount.py
+++ baelish/currency/amount.py
@@ -106,15 +106,3 @@ class Amount:
             copperPennies = copperPennies * -1
 
         return Amount(goldDragons, silverStags, copperPennies)
-
-
-class Coin(float):
-    def __new__(self, longUnit, shortUnit, quantity=0):
-        return super().__new__(self, quantity)
-
-    def __init__(self, longUnit, shortUnit, quantity=0):
-        self._longUnit = longUnit
-        self._shortUnit = shortUnit
-
-    def __str__(self, useLongUnit=False, formatSpecifier=".2f"):
-        return "{:{formatSpecifier}} {}".format(self, self._longUnit if useLongUnit else self._shortUnit, formatSpecifier=formatSpecifier)

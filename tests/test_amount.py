8f40e335177bb14d4aeb7d09eef2aa4d284ab0ef
gariepinus <mail@gariepinus.de>
2022-10-27 20:14:51 +0200

    TypeError assertion for Amount

--- tests/test_amount.py
+++ tests/test_amount.py
@@ -1,11 +1,19 @@
 """Tests for Amount class."""
+import pytest
 import baelish.currency
+
+
+def test_exceptions():
+    """Assert Quantity raises TypeErrors when non-ints get passed to num."""
+    with pytest.raises(TypeError):
+        baelish.currency.Amount(silver_stags=0.5)
+
 
 def test_str():
     """Assert the str method works as intendet."""
     amt = baelish.currency.Amount()
     assert f"{amt}" == "0 CP"
-    amt = baelish.currency.Amount(1, 0, 0)
+    amt = baelish.currency.Amount(gold_dragons=1)
     assert f"{amt}" == "1 GD"
     amt = baelish.currency.Amount(0, 20, 0)
     assert f"{amt}" == "20 SS"
@@ -18,12 +26,14 @@ def test_str():
     amt = baelish.currency.Amount(0, 50, 1)
     assert f"{amt}" == "50 SS, 1 CP"
 
+
 def test_getset_gd():
     """Assert getter and setter for property gold_dragons work as intendet."""
     amt = baelish.currency.Amount()
     amt.gold_dragons = -1000
     assert int(amt.gold_dragons) == -1000
     assert f"{amt.gold_dragons}" == "-1,000 GD"
+
 
 def test_getset_ss():
     """Assert getter and setter for property silver_stags work as intendet."""
@@ -32,12 +42,14 @@ def test_getset_ss():
     assert int(amt.silver_stags) == 666
     assert f"{amt.silver_stags}" == "666 SS"
 
+
 def test_getset_cp():
     """Assert getter and setter for property copper_pennies work as intendet."""
     amt = baelish.currency.Amount()
     amt.copper_pennies = 3
     assert int(amt.copper_pennies) == 3
     assert f"{amt.copper_pennies}" == "3 CP"
+
 
 def test_convert_cp():
     """Assert that conversion of whole amount in cp works as intendet."""
